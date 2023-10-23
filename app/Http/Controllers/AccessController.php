b126fc12a249ca1542834cde28d39fdd676d7ec8
Julian Rodriguez <bjrm0293@gmail.com>
2023-10-22 19:36:36 -0500

    Update Local to Gitlab

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -18,7 +18,7 @@ class AccessController extends Controller
         $validator = Validator::make($request->all(),[
             // 'name' => 'required string max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string min:8'
+            'password' => 'required|string|min:8'
         ]);
 
         if( $validator->fails() )
